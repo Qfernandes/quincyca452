5bb343e5636ec175a10c9a4c1701a76a441dbf66
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2022-11-07 11:12:34 +0000

    updated

--- Assignments/Main.java
+++ Assignments/Main.java
@@ -13,13 +13,56 @@ public class Main
         Songs friends = new Songs("Friends","Anne Marie", 22222889);
         //friends.print();
 
+
+        Songs song2 = new Songs("Don't Start Now","Dua Lipa",6070228 );
+        //song2.print();
+
+        Songs song3 = new Songs("Calm Down","Ed Sheeran",291554201 );
+        //song3.print();
+
+        Songs song4 = new Songs("Physcho","Anne Marie",517191452 );
+        //song4.print();
+
+        Songs song5 = new Songs("Anti-hero","Taylor Swift",957123 );
+        //song5.print();
+
+        Songs song6 = new Songs("As it was","Harry Styles",432178954 );
+        //song6.print();
+
+        Songs song7 = new Songs("Dandelions","Ruth B",77754632 );
+        //song7.print();
+
+        Songs song8 = new Songs("Our Song","Anne Marie",673245624 );
+        //song8.print();
+
+        Songs song9 = new Songs("Location","Dave",96549778 );
+        //song9.print();
+
+        Songs song10 = new Songs("Danza Kuduro","Don Omar",890432156 );
+
+        //song10.print();
+        Songs song11 = new Songs("Beautiful","Ed Sheeran",890500156 );
+        //song10.print();
+
         ArrayList<Songs> songlist = new ArrayList<Songs>();
         songlist.add(friends);
+        songlist.add(song2);
+        songlist.add(song3);
+        songlist.add(song4);
+        songlist.add(song5);
+        songlist.add(song6);
+        songlist.add(song7);
+        songlist.add(song8);
+        songlist.add(song9);
+        songlist.add(song10);
+
+
+
 
         for (Songs song : songlist)
         {
             song.print();
         }
-        
+
     }
 }
